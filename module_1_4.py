6f0932a0333b9895acd993666a4ac1f23ada9b28
Vlad <banshchikovvl@yandex.ru>
2024-08-28 17:54:26 +0700

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,10 @@
+#1 Организация программ и методы строк
+my_string = input()
+print(len(my_string))
+
+#2 Работа с методами строк
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ','' ))
+print(my_string[0])
+print(my_string[-1])
